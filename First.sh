a8f26093ce1195db9696b52c0f846472f56e2ac2
grux-zilla <gojirafan35@gmail.com>
2018-03-02 10:53:55 -0500

    raw upload

--- First.sh
+++ First.sh
@@ -8,7 +8,7 @@ apt-get update && apt-get upgrade -y
 # sequence on the bottom half of the script     #
 #################################################
 
-addRepos (){
+addRepos() {
     add-apt-repository ppa:webupd8team/java -y
     add-apt-repository ppa:webupd8team/atom -y
     add-apt-repository ppa:obsproject/obs-studio -y
@@ -17,10 +17,34 @@ addRepos (){
 
 apt-get update
 
-addApps(){
-  apt-get install vlc -y
-  apt-get install krita -y
+addApps() {
+  apt-get install vlc krita atom ffmpeg obs-studio redbook -y
+
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+
+  apt-get install oracle-java8-installer
 
 }
 
+addChrome() {
+  echo "deb [arch=amdx64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
+
+  wget https://dl.google.com/linux/linux_signing_key.pub
+  apt-key add linux_signing_key.pub
+  apt-get update
+  apt-get install google-chrome-stable
+
+}
+
+addPapers() {
+
+  mv "$(dirname $0)/Wallpaper/*.jpg" /Home/Pictures
+}
+
+addApps;
+
 addRepos;
+
+addChrome;
+
+addPapers;
